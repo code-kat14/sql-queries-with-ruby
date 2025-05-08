f52ca40529f0164b6ee552e99136ef5a08e045bf
katie <katienino0@gmail.com>
2025-05-08 09:45:56 -0700

    sql querying for reviewers with 7+ stars

--- my_allocine.rb
+++ my_allocine.rb
@@ -6,7 +6,7 @@ requests['Display reviewer name from the reviews table'] = "SELECT rev_name FROM
 requests["Find the year when the movie American Beauty released"] = "SELECT mov_dt_rel FROM movies WHERE mov_title='American Beauty';"
 requests["Find the movie which was released in the year 1999"] = "SELECT mov_title FROM movies WHERE strftime('%Y', mov_dt_rel)='1999';"
 requests["Find the movie which was released before 1998"] = "SELECT mov_title FROM movies WHERE strftime('%Y', mov_dt_rel)<'1998';"
-requests["Find the name of all reviewers who have rated 7 or more stars to their rating order by reviews rev_name (it might have duplicated names :-))"] = ""
+requests["Find the name of all reviewers who have rated 7 or more stars to their rating order by reviews rev_name (it might have duplicated names :-))"] = "SELECT reviews.rev_name FROM reviews JOIN movies_ratings_reviews ON reviews.id = movies_ratings_reviews.rev_id ORDER BY rev_name;"
 requests["Find the titles of the movies with ID 905, 907, 917"] = ""
 requests["Find the list of those movies with year and ID which include the words Boogie Nights"] = ""
 requests["Find the ID number for the actor whose first name is 'Woody' and the last name is 'Allen'"] = ""
